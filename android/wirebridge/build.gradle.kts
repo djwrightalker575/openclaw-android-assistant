91f26663086fd234e8a34de6b33ca42bfbcd8cb0
djwrightalker575 <jamesbwright88130@gmail.com>
2026-08-01 00:29:57 -0600

    Bump WIRE bridge version for DNS proxy repair

--- android/wirebridge/build.gradle.kts
+++ android/wirebridge/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.uwright.wirebridge"
         minSdk = 24
         targetSdk = 28
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
