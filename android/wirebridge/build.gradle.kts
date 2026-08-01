18ae57173e0c4d1144c1d83ad34d7eeadca653fd
djwrightalker575 <jamesbwright88130@gmail.com>
2026-08-01 01:03:12 -0600

    Bump WIRE Bridge version for TLS trust fix

--- android/wirebridge/build.gradle.kts
+++ android/wirebridge/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.uwright.wirebridge"
         minSdk = 24
         targetSdk = 28
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     buildTypes {
